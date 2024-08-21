a8c01b3a0d96db6728dc18456a3e4cd01505591b
Johann Grabner <jgrabner@business-tuning.at>
2024-08-21 09:10:36 +0200

    Aufgabe Produkte

--- src/Connect.java
+++ src/Connect.java
@@ -34,6 +34,7 @@ public class Connect {
 
 
     public static void readCustomer() {
+        //Geschäftslogik / Business
         Connection conn = null;
         try {
             // db parameters
@@ -71,6 +72,7 @@ public class Connect {
                     System.out.println("Bonuspunkte undefiniert");
                 }
             }
+            rs.close();
 
         } catch (SQLException e) {
             System.out.println(e.getMessage());
@@ -83,5 +85,15 @@ public class Connect {
                 System.out.println(ex.getMessage());
             }
         }
+    }
+
+    public void readAllProducts(){
+        /* 20 Minuten Beispiel, danach 20 Minuten Pause, Treffen um 09:50 Uhr
+        1. Create Table SQLite Studio -- ProduktNr, Bezeichnung NettoPreis UstSatz
+        2. ein paar Produkte einfügen, teilweise auch mit NULL Werten
+        3. Mit Hilfe von JDBC alle Proukte absteigen sortiert nach Preis ausgeben
+        4. zusätzlich eine berechnet Spalte mit BruttoBetrag    Netto: 200 UstSatzProz: 20 Bruttobetrag: 240
+        5. Berechnung von Brutto entwedwer im SELECT-Statement oder mit "Java"
+         */
     }
 }
